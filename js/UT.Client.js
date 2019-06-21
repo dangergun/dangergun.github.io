a03ee19b266f3cb313727d02d882da3a855b5303
dangergun <31354814+dangergun@users.noreply.github.com>
2019-06-20 23:33:45 -0600

    Update UT.Client.js

--- js/UT.Client.js
+++ js/UT.Client.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
     $(".copy-link").click(function(){
         var temp = $("<input>");
         $("body").append(temp);
-        temp.val("https://dangergun.github.io/client").select();
+        temp.val("https://dangergun.github.io/UTReborn.swf").select();
         document.execCommand("copy");
         temp.remove();
         $(".copy-link").text("(Copied!)")
