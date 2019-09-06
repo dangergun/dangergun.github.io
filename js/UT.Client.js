23697edcaa8772b5eb49d7a27edc327665e09fb0
EpicQuackIV <49463775+EpicQuackIV@users.noreply.github.com>
2019-09-06 02:32:34 -0400

    changed webclient flash link

--- js/UT.Client.js
+++ js/UT.Client.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
     $(".copy-link").click(function(){
         var temp = $("<input>");
         $("body").append(temp);
-        temp.val("https://dangergun.github.io/UTReborn.swf").select();
+        temp.val("https://dangergun.github.io/UTReborn.swf?build=release").select();
         document.execCommand("copy");
         temp.remove();
         $(".copy-link").text("(Copied!)")
